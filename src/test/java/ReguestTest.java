36b2816adf8b782518a32b900f3e3ccac014db80
Юлия Фёдорова <uliafedorova297@gmail.com>
2022-10-09 00:14:31 +0300

    add src/test/java/ReguestTest.java

--- src/test/java/ReguestTest.java
+++ src/test/java/ReguestTest.java
@@ -20,7 +20,7 @@ public class ReguestTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data") );
+                .body("data", equalTo(" data") );
     }
 
 
